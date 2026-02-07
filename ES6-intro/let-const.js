871ae32ed5e40d6871114766e99e3085aa5185f4
NahidWorld <fahimbinimam@gmail.com>
2026-02-07 18:15:46 +0600

    let and constant practice

---
+++ ES6-intro/let-const.js
@@ -0,0 +1,13 @@
+const name = 'Nahid';
+console.log(name);
+
+// console.log(username);
+// const username = 'Nahid Fahim';
+
+console.log(vari);
+var vari = 'Nahid Fahim';
+
+const dress = ['shirt', 'pant', 'jacket'];
+console.log(dress);
+dress.push('mafler');
+console.log(dress);
