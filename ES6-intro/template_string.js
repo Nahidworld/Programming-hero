2287b6fae2af2712309a8cd3d4d6555f73488976
NahidWorld <fahimbinimam@gmail.com>
2026-02-14 21:35:22 +0600

    template string

---
+++ ES6-intro/template_string.js
@@ -0,0 +1,10 @@
+const poem = `I love my Country.
+I love my Country`;
+console.log(poem);
+
+function sum(num1, num2){
+    const result = num1 + num2;
+    const output = 'Sum of '+ num1 + ' and ' + num2 + ' is equal to ' + result;
+    console.log(output);
+}
+sum(10, 20);
