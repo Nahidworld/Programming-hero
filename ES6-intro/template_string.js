120180a61d4e39a2fe7c08b1026660710938fe14
NahidWorld <fahimbinimam@gmail.com>
2026-02-17 00:00:30 +0600

    template string

--- ES6-intro/template_string.js
+++ ES6-intro/template_string.js
@@ -4,7 +4,10 @@ console.log(poem);
 
 function sum(num1, num2){
     const result = num1 + num2;
-    const output = 'Sum of '+ num1 + ' and ' + num2 + ' is equal to ' + result;
+    
+    // const output = 'Sum of '+ num1 + 'and ' + num2 + ' is equal to ' + result;
+    const output = `sum of ${num1} and ${num2} is euqal to ${result}`;
     console.log(output);
+    
 }
 sum(10, 20);
