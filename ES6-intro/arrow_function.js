e36b66dd5ed09cf3041442077cf60e5b04331c2d
NahidWorld <fahimbinimam@gmail.com>
2026-03-03 13:16:49 +0600

    arrow function

---
+++ ES6-intro/arrow_function.js
@@ -0,0 +1,16 @@
+//function declaration
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+// addition(3,19);
+console.log(addition(3,19));
+
+
+//function expression
+const add = function(num1, num2){
+    return num1 + num2;
+}
+
+const result = add(29,100);
+console.log(result);
